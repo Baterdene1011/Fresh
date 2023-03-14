a35882d2baf742cdcf3903def428d108acb62f0d
Baterdene1011 <63986100+Baterdene1011@users.noreply.github.com>
2023-03-14 17:08:29 +0800

    firstCommit

---
+++ pages/about/page.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>
+        <h1>about page</h1>
+        <Link href={'/'}></Link>
+    </div>
+  )
+}
